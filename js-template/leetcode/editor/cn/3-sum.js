69b05604302c00d8fd847e5ce238c58a7090f4bc
SCP5637 <SCP5637@users.noreply.github.com>
2026-03-18 10:05:30 +0800

    test: #15&#18 n数和问题

--- js-template/leetcode/editor/cn/3-sum.js
+++ js-template/leetcode/editor/cn/3-sum.js
@@ -1,8 +1,8 @@
 /*
- * @lc app=leetcode.cn id=18 lang=javascript
+ * @lc app=leetcode.cn id=15 lang=javascript
  * @lcpr version=30400
  *
- * [18] 四数之和
+ * [15] 三数之和
  */
 
 
@@ -12,20 +12,24 @@ import {TreeNode} from "../common/treeNode.js";
 // @lc code=start
 /**
  * @param {number[]} nums
- * @param {number} target
  * @return {number[][]}
  */
-var fourSum = function(nums, target) {
+var threeSum = function(nums) {
     nums.sort((a, b) => a - b);
-    return nSumTarget(nums, 4, 0, target);
+    return nSumTarget(nums, 3, 0, 0);
 };
 
-// n数合问题，解读见#15
+// 实际该问题更像是 n 数之和的分支，统一写出n数找和方法，4数和见#18
+// nums: 操作数组对象
+// n: n数
+// start: 求和起始索引，一般都是nums0找到nums尾
+// target: 目标和数值
 function nSumTarget(nums, n, start, target) {
     const sz = nums.length;
     let res = [];
     if (n < 2 || sz < n) return res;
     if (n === 2) {
+        // 双指针那一套操作
         let low = start, high = sz - 1;
         while (low < high) {
             const sum = nums[low] + nums[high];
@@ -41,9 +45,11 @@ function nSumTarget(nums, n, start, target) {
             }
         }
     } else {
+        // n>2 时，递归(n-1)Sum
         for (let i = start; i < sz; i++) {
             const sub = nSumTarget(nums, n - 1, i + 1, target - nums[i]);
             for (let arr of sub) {
+                // (n-1)Sum 加上 nums[i] 就是 nSum
                 arr.push(nums[i]);
                 res.push(arr);
             }
@@ -62,11 +68,15 @@ function nSumTarget(nums, n, start, target) {
 
 /*
 // @lcpr case=start
-// [1,0,-1,0,-2,2]\n0\n
+// [-1,0,1,2,-1,-4]\n
 // @lcpr case=end
 
 // @lcpr case=start
-// [2,2,2,2,2]\n8\n
+// [0,1,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,0,0]\n
 // @lcpr case=end
 
  */
