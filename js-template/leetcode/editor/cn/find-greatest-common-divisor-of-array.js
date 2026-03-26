aefcf36085f7386cf31e960563af8274c6e126d0
SCP5637 <SCP5637@users.noreply.github.com>
2026-03-26 16:33:57 +0800

    test: #1979 递归最大公因数

---
+++ js-template/leetcode/editor/cn/find-greatest-common-divisor-of-array.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=1979 lang=javascript
+ * @lcpr version=30401
+ *
+ * [1979] 找出数组的最大公约数
+ */
+
+
+import {ListNode} from "../common/listNode.js";
+import {TreeNode} from "../common/treeNode.js";
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+    let max = Math.max(...nums);
+    let min = Math.min(...nums);
+    return gcd(max, min);
+};
+
+// 辗转相除法递归最大公因数
+var gcd = function(a, b) {
+    if (b === 0) return a;
+    return gcd(b, a % b);
+}
+// @lc code=end
+
+// your test code here
+
+
+
+
+
+/*
+// @lcpr case=start
+// [2,5,6,9,10]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,5,6,8,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,3]\n
+// @lcpr case=end
+
+ */
+
