fdadf574efeb1970f5c3f8b66d177ce8628a6e8d
SCP5637 <SCP5637@users.noreply.github.com>
2026-03-26 10:22:42 +0800

    test: #95 二叉搜索树结构输出，逻辑思考

--- js-template/leetcode/editor/cn/unique-binary-search-trees-ii.js
+++ js-template/leetcode/editor/cn/unique-binary-search-trees-ii.js
@@ -25,8 +25,14 @@ import {TreeNode} from "../common/treeNode.js";
 var generateTrees = function(n) {
     if (n === 0) return [];
     const memo = new Map();
+    
+    // 自顶向下dp，build发起递归lo到hi区间，遍历lo到hi，取root
+    // root左边的节点建立子树，区间是root左边的区间，右侧同理，如果空就返回null
+    // 所有root遍历完，也就构建完了，注意res非基例时只 push TreeNode 实例，避免答案数组乱套
+    
     function build(lo, hi) {
         const key = lo + ',' + hi;
+        // 取memo记忆，避免递归爆炸
         if (memo.has(key)) return memo.get(key);
         const res = [];
         if (lo > hi) {
