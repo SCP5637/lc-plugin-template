c4228cf2c4b7852877301a423703857ccaaa2680
SCP5637 <SCP5637@users.noreply.github.com>
2026-04-29 09:46:25 +0800

    test: #9

--- js-template/leetcode/editor/cn/palindrome-number.js
+++ js-template/leetcode/editor/cn/palindrome-number.js
@@ -15,7 +15,21 @@ import {TreeNode} from "../common/treeNode.js";
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+    // 负数不是回文数
+    // 如果数字的最后一位是0，那么只有当数字是0时才是回文数
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+        return false;
+    }
     
+    let reversedHalf = 0;
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    
+    // 当数字长度为奇数时，可以通过 reversedHalf / 10 去除中间位
+    // 当数字长度为偶数时，x === reversedHalf
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
 };
 // @lc code=end
 
