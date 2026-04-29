afd6f7d78d57c00a57abf2242e90795b27da16b6
SCP5637 <SCP5637@users.noreply.github.com>
2026-04-29 10:03:50 +0800

    test: #1 两数之和

--- js-template/leetcode/editor/cn/two-sum.js
+++ js-template/leetcode/editor/cn/two-sum.js
@@ -16,8 +16,35 @@ import {TreeNode} from "../common/treeNode.js";
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
 };
+// var twoSum = function(nums, target) {
+//     // 双指针方案
+//     // 保存原始下标，然后按值排序
+//     const indexedNums = nums.map((val, idx) => ({ val, idx }));
+//     indexedNums.sort((a, b) => a.val - b.val);
+    
+//     let left = 0, right = indexedNums.length - 1;
+//     while (left < right) {
+//         const sum = indexedNums[left].val + indexedNums[right].val;
+//         if (sum === target) {
+//             return [indexedNums[left].idx, indexedNums[right].idx];
+//         } else if (sum < target) {
+//             left++;
+//         } else {
+//             right--;
+//         }
+//     }
+//     return [];
+// };
 // @lc code=end
 
 // your test code here
