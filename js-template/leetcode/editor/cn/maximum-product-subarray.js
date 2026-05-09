aac9b9b3d202188706f3d9d8b1e95ca821083109
SCP5637 <SCP5637@users.noreply.github.com>
2026-05-09 09:19:41 +0800

    test: #152

--- js-template/leetcode/editor/cn/maximum-product-subarray.js
+++ js-template/leetcode/editor/cn/maximum-product-subarray.js
@@ -14,20 +14,33 @@ import {TreeNode} from "../common/treeNode.js";
  * @param {number[]} nums
  * @return {number}
  */
+// var maxProduct = function(nums) {
+//     let dpMax = new Array(nums.length);
+//     let dpMin = new Array(nums.length);
+//     dpMax[0] = dpMin[0] = nums[0];
+//     let max = nums[0];
+    
+//     for(let i = 1; i < nums.length; i++){
+//         // 当前数是正数：max×num 最大，min×num 最小
+//         // 当前数是负数：min×num 最大，max×num 最小
+//         dpMax[i] = Math.max(dpMax[i-1] * nums[i], dpMin[i-1] * nums[i], nums[i]);
+//         dpMin[i] = Math.min(dpMax[i-1] * nums[i], dpMin[i-1] * nums[i], nums[i]);
+//         max = Math.max(max, dpMax[i]);
+//     }
+//     return max;
+// };
 var maxProduct = function(nums) {
-    let dpMax = new Array(nums.length);
-    let dpMin = new Array(nums.length);
-    dpMax[0] = dpMin[0] = nums[0];
-    let max = nums[0];
+    let maxProd = nums[0];
+    let minProd = nums[0];
+    let result = nums[0];
     
     for(let i = 1; i < nums.length; i++){
-        // 当前数是正数：max×num 最大，min×num 最小
-        // 当前数是负数：min×num 最大，max×num 最小
-        dpMax[i] = Math.max(dpMax[i-1] * nums[i], dpMin[i-1] * nums[i], nums[i]);
-        dpMin[i] = Math.min(dpMax[i-1] * nums[i], dpMin[i-1] * nums[i], nums[i]);
-        max = Math.max(max, dpMax[i]);
+        let temp = maxProd;
+        maxProd = Math.max(maxProd * nums[i], minProd * nums[i], nums[i]);
+        minProd = Math.min(temp * nums[i], minProd * nums[i], nums[i]);
+        result = Math.max(result, maxProd);
     }
-    return max;
+    return result;
 };
 // @lc code=end
 
