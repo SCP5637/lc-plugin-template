8b3e9fda0190739ed9659318dd53d08bfb5231ae
SCP5637 <SCP5637@users.noreply.github.com>
2026-05-09 09:18:37 +0800

    test: #152 dp正负数细节

---
+++ js-template/leetcode/editor/cn/maximum-product-subarray.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=152 lang=javascript
+ * @lcpr version=30403
+ *
+ * [152] 乘积最大子数组
+ */
+
+
+import {ListNode} from "../common/listNode.js";
+import {TreeNode} from "../common/treeNode.js";
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let dpMax = new Array(nums.length);
+    let dpMin = new Array(nums.length);
+    dpMax[0] = dpMin[0] = nums[0];
+    let max = nums[0];
+    
+    for(let i = 1; i < nums.length; i++){
+        // 当前数是正数：max×num 最大，min×num 最小
+        // 当前数是负数：min×num 最大，max×num 最小
+        dpMax[i] = Math.max(dpMax[i-1] * nums[i], dpMin[i-1] * nums[i], nums[i]);
+        dpMin[i] = Math.min(dpMax[i-1] * nums[i], dpMin[i-1] * nums[i], nums[i]);
+        max = Math.max(max, dpMax[i]);
+    }
+    return max;
+};
+// @lc code=end
+
+// your test code here
+
+
+
+
+
+/*
+// @lcpr case=start
+// [2,3,-2,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-2,3,-1]\n
+// @lcpr case=end
+
+ */
+
